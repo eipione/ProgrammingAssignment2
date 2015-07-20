4ed947330ea2b2dcc209bb5474e05d2ca1349221
Misam Abbas <github@movieanalytics.com>
2015-07-19 21:55:40 -0400

    Inverse Caching Matrix Wrapper

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file provides a mechanism for caching the inverse of a matrix
+## to avoid recomputation.
+##
+## This is accomplished by creating an R object capable to storing its own inverse
+##
+## To use this, 
+## First create the inverse caching R object by calling the makeCacheMatrix function
+## Then call the cacheSolve (possibly multiple times)
 
-## Write a short comment describing this function
-
+## Creates an R object which is a wrapper over a matrix and capable of caching its inverse 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setInv <- function(inv) {
+        inv <<- inv
+    }
+    getInv <- function() {
+        inv 
+    }
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of the matrix given the R object created by the makeCacheMatrix function
+## The actual computation is done on the first call(with a particular object), 
+## the subsequent calls return the cached value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    ## Return a matrix that is the inverse of 'data'
+    inv <- solve(data, ...)
+    x$setInv(inv)
+    inv
 }
